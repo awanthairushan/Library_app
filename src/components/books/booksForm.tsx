7aa78598572fb78023d34a89b4209057d5fa261c
hbsbashitha <hbsbashitha@gmail.com>
2022-01-19 16:23:31 +0530

    add basics in the books form

---
+++ src/components/books/booksForm.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Row, Col, Container, Button } from "react-bootstrap";
+import { AiOutlineCloseCircle} from "react-icons/ai";
+
+const BooksForm: React.FC = () => {
+  return (
+    <Row className="booksFormSection  ">
+      <Col xs={12}>
+          
+        <h1>Create Book   </h1>
+        <AiOutlineCloseCircle/>
+      </Col>
+
+      <Col xs={12}>
+          <Container className="booksForm ml-3">
+          <Form  className="ml-3">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Title of the Book</Form.Label>
+            <Form.Control type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>ISBN</Form.Label>
+            <Form.Control type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Author</Form.Label>
+
+            <Form.Select aria-label="Default select example">
+              <option></option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Button variant="primary" type="submit" className="end-0 mr-0">
+            Create
+          </Button>
+        </Form>
+          </Container>
+          
+
+      </Col>
+    </Row>
+  );
+};
+
+export default BooksForm;
